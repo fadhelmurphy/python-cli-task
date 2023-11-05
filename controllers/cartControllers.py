fa866f9f2eb56545e1ba522d1bc3de89355a7b9f
Fadhel ngepush 🚀 <fadhelijlalfalah@gmail.com>
2023-11-05 21:29:37 +0700

    feat: add postCart Services

--- controllers/cartControllers.py
+++ controllers/cartControllers.py
@@ -1,6 +1,8 @@
 from services.cartServices import GetCarts
+from services.productServices import showlist_product
+
 def GetCartsController():
     carts = GetCarts()
-    print(carts)
+    showlist_product(carts)
 
     
